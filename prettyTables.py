8c0ef6d45eee2673ffa5f440feebb5edb8c2c0d4
karmaUdel <karma@udel.edu>
2017-12-03 20:34:05 -0500

    Adding average precision and recall calculations

--- prettyTables.py
+++ prettyTables.py
@@ -3,6 +3,17 @@ from prettytable import PrettyTable
 file = open('configurations.csv', 'r')
 reader = csv.reader(file)
 t = PrettyTable(['Configuration', 'Precision', 'Recall','True positive','True negative','False positive','False negative'])
+prec = 0
+rec = 0
+i = 0
 for row in reader:
     t.add_row([row[0],row[2], row[1],row[3],row[4],row[5],row[6]])
+    if i <13 :
+        prec += float(row[2])
+        rec += float(row[1])
+    i = i+1
+
+print "Average precision : " + str(prec/13)
+print "Average recall : " + str(rec/13)
+
 print t
