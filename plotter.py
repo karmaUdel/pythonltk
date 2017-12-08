8ac4611b8bafb52f90bd7d96e086fce8b1ede680
karmaUdel <karma@udel.edu>
2017-12-08 14:20:48 -0500

    Adding barcharts for metrics

--- plotter.py
+++ plotter.py
@@ -3,6 +3,7 @@ import csv
 import pylab
 from matplotlib.backends.backend_pdf import PdfPages
 import datetime
+import numpy as np
 
 
 def plotgraph(values,ylabel, xlabel,xvalues,name):
@@ -53,12 +54,57 @@ def addinfo(pdf):
     d['ModDate'] = datetime.datetime.today()
 
 
+def barcharts(xvalues,values):
+    n_groups = len(xvalues)
+
+    means_men = values[0]
+    #std_men = (2, 3, 4, 1, 2)
+
+    means_women = values[1]
+    # std_women = (3, 5, 2, 3, 3)
+
+    fig, ax = plt.subplots()
+
+    index = np.arange(n_groups)
+    bar_width = 0.45
+
+    opacity = 0.4
+    error_config = {'ecolor': '0.3'}
+
+    rects1 = plt.bar(index, means_men, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     # yerr=std_men,
+                     error_kw=error_config,
+                     label='Precision')
+
+    rects2 = plt.bar(index + bar_width, means_women, bar_width,
+                     alpha=opacity,
+                     color='r',
+                     # yerr=std_women,
+                     error_kw=error_config,
+                     label='Recall')
+
+    plt.xlabel('Configurations')
+    plt.ylabel('Scores')
+    plt.title('Scores by Configuration')
+    plt.xticks(index + bar_width/2 , xvalues)#('Conf 1', 'Conf 2', 'Conf 3', 'Conf 4', 'Conf 5', 'Conf 6', 'Conf 7', 'Conf 8', 'Conf 9', 'Conf 10','Conf 11','Conf 12', 'Conf 13'))
+    plt.legend()
+
+    plt.tight_layout()
+    fig1=plt.gcf()
+    pylab.draw()
+    fig1.savefig('barcharts.pdf',format="pdf")
+
+    plt.show()
+
+
 yvalues = []
 # xvalues = ["bad_5Weight100bi_0uni", "bad_5Weight100bi_20uni", "bad_10Weight100bi_0uni", "bad_10Weight100bi_20uni",
 #           "bad_100Weight100bi_20uni", "bad_100Weight25bi_0uni", "bad_100Weight25bi_20uni",
 #           "bad_100Weight50bi_0uni", "bad_100Weight50bi_20uni", "bad_100Weight75bi_0uni", "bad_100Weight75bi_20uni",
 #           "bad_100Weight100bi_0uni", "bad_100Weight100bi_20uni"]
-xvalues = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+xvalues = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 file = open('configurations.csv', 'r')
 reader = csv.reader(file)
 print "plot1"
@@ -78,7 +124,8 @@ file.close()
 file = open('configurations.csv', 'r')
 reader = csv.reader(file)
 y2values = getValues("recall",reader,y2values)
-plotgraph([yvalues,y2values], "","configurations",xvalues,name)
-file.close()
+#plotgraph([yvalues,y2values], "","configurations",xvalues,name)
+#file.close()
+barcharts(xvalues,[yvalues,y2values])
 # reader.close()
 # pp.savefig(plot3)
